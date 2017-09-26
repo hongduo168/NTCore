063da1153439db4479602c0bd536989deff1263e
liuhonguo <hongduo168@126.com>
2017-09-26 10:26:10 +0800

    fixed code first

--- NTCore.DataModel/BaseEntity.cs
+++ NTCore.DataModel/BaseEntity.cs
@@ -16,41 +16,42 @@ namespace NTCore.DataModel
         /// Id
         /// </summary>
         //[DataMember]
-        [Column("id")]
-        [Key]
-        public long Id { get; set; }
+        [Key, DefaultValue(0), Column("id")]
+        public int Id { get; set; }
 
         /// <summary>
         /// 创建时间
         /// </summary>
         //[DataMember(Name = "create_time", EmitDefaultValue = true)]
-        [Column("create_time")]
+        [Required, DefaultValue("getdate()"), Column("create_time")]
         public DateTime CreateTime { get; set; }
 
         /// <summary>
         /// 更新时间
         /// </summary>
         //[DataMember(Name = "update_time")]
-        [Column("update_time")]
+        [Required, DefaultValue("getdate()"), Column("update_time")]
+
         public DateTime UpdateTime { get; set; }
 
-        [Column("creator_id")]
+        [Required, DefaultValue(0), Column("creator_id")]
         public int CreatorId { get; set; }
 
-        [Column("updater_id")]
+
+        [Required, DefaultValue(0), Column("updater_id")]
         public int UpdaterId { get; set; }
 
         /// <summary>
         /// 状态
         /// </summary>
         //[DataMember(Name = "data_state", EmitDefaultValue = true)]
-        [Column("data_state")]
+        [Required, DefaultValue(0), Column("data_state")]
         public EnumState DataState { get; set; }
     }
 
     public partial class HotelEntity : BaseEntity
     {
-        [Column("hotel_id")]
+        [Required, DefaultValue(0), Column("hotel_id")]
         public int HotelId { get; set; }
     }
 
